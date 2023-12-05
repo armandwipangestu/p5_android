56d1e90820cba255b6a191ba635a186944a13664
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-12-05 19:26:48 +0700

    update: add some car and change color schema to yellow

--- tugas/UAS_RENTAL_MOBIL_UPDATE/app/src/main/java/com/tugas/uas_rentalmobil/Transaction.java
+++ tugas/UAS_RENTAL_MOBIL_UPDATE/app/src/main/java/com/tugas/uas_rentalmobil/Transaction.java
@@ -1,23 +1,13 @@
 package com.tugas.uas_rentalmobil;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Transaction extends BaseActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_transaction);
-//        bottomNavigationView = findViewById(R.id.bottomNavigation);
-//        bottomNavigationView.setOnItemSelectedListener(navigation);
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     int getLayoutId() {
